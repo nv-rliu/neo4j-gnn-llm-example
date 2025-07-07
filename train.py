c1879c4a682c58ce0267085c8e85b6f4d47a9ce2
Ralph Liu <ralphl@nvidia.com>
2025-07-07 19:55:19 +0000

    Changes

--- train.py
+++ train.py
@@ -1,10 +1,8 @@
 import argparse
 import math
 import os
-import re
 import time
 
-import pandas as pd
 import torch
 from dotenv import load_dotenv
 from torch_geometric.loader import DataLoader
@@ -19,7 +17,7 @@ from tqdm import tqdm
 
 from compute_metrics import compute_metrics
 
-from STaRKQADataset import STaRKQADataset
+from STaRKQADatasetGDS import STaRKQADataset
 from STaRKQAVectorSearchDataset import STaRKQAVectorSearchDataset
 
 def get_loss(model, batch, model_save_name) -> Tensor:
@@ -48,9 +46,9 @@ def save_params_dict(model, save_path):
             del state_dict[k]  # Delete parameters that do not require gradient
     torch.save(state_dict, save_path)
 
-
 def load_params_dict(model, save_path):
-    state_dict = torch.load(save_path)
+    state_dict = model.state_dict()
+    state_dict.update(torch.load(save_path)) #All weights might not be saved, eg when using LoRA.
     model.load_state_dict(state_dict)
     return model
 
@@ -67,6 +65,7 @@ def train(
     algo_config_version,
     g_retriever_config_version,
     checkpointing=False,
+    sys_prompt=None
 ):
     def adjust_learning_rate(param_group, LR, epoch):
         # Decay the learning rate with half-cycle cosine after warmup
@@ -134,11 +133,23 @@ def train(
     )
 
     if llama_version == 'tiny_llama':
-        llm = LLM(model_name='TinyLlama/TinyLlama-1.1B-Chat-v0.1', num_params=1)
+        llm = LLM(
+            model_name='TinyLlama/TinyLlama-1.1B-Chat-v0.1',
+            num_params=1,
+            sys_prompt=sys_prompt
+        )
     elif llama_version == 'llama2-7b':
-        llm = LLM(model_name='meta-llama/Llama-2-7b-chat-hf', num_params=7)
+        llm = LLM(
+            model_name='meta-llama/Llama-2-7b-chat-hf',
+            num_params=7,
+            sys_prompt=sys_prompt
+        )
     elif llama_version == 'llama3.1-8b':
-        llm = LLM(model_name='meta-llama/Llama-3.1-8B-Instruct', num_params=8)
+        llm = LLM(
+            model_name='meta-llama/Llama-3.1-8B-Instruct',
+            num_params=8,
+            sys_prompt=sys_prompt
+        )
 
 
     if args.freeze_llm:
@@ -211,7 +222,7 @@ def train(
             best_epoch = epoch
             save_params_dict(model, f'{root_path}/models/{retrieval_config_version}_{algo_config_version}_{g_retriever_config_version}_{model_save_name}_best_val_loss_ckpt.pt')
 
-    if llm.device != "cpu":
+    if llm.device.type != "cpu":
         torch.cuda.empty_cache()
         torch.cuda.reset_max_memory_allocated()
 
@@ -261,7 +272,14 @@ if __name__ == '__main__':
     parser.add_argument('--algo_config_version', type=int, required=True)
     parser.add_argument('--g_retriever_config_version', type=int, required=True)
     parser.add_argument('--freeze_llm', type=bool, default=False)
+    default_llm_prompt = (
+        "You are an expert assistant that can answer "
+        "any question from its knowledge, given a knowledge graph embedding and "
+        "it's textualized context. Just give the answer, without explanation."
+    )
+    parser.add_argument('--sys_prompt', type=str, default=default_llm_prompt)
     args = parser.parse_args()
+    breakpoint()
     load_dotenv('db.env', override=True)
 
     start_time = time.time()
@@ -277,5 +295,6 @@ if __name__ == '__main__':
         algo_config_version=args.algo_config_version,
         g_retriever_config_version=args.g_retriever_config_version,
         checkpointing=args.checkpointing,
+        sys_prompt=args.sys_prompt
     )
     print(f"Total Time: {time.time() - start_time:2f}s")
