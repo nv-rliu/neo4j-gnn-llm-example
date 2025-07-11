bd393896da7c29c335f8ce76b2262c66d452cd7f
Ralph Liu <ralphl@nvidia.com>
2025-07-11 17:45:01 +0000

    Remove param arg

--- train.py
+++ train.py
@@ -132,12 +132,12 @@ def train(
         num_layers=num_gnn_layers,
         heads=4,
     )
-	
+    
 
     common_args = {
         'sys_prompt': sys_prompt,
         'n_gpus': num_gpus	
-	}
+    }
 
     print("=====")
     print(f"common_args: {common_args}")
@@ -146,20 +146,17 @@ def train(
     if llama_version == 'tiny_llama':
         llm = LLM(
             model_name='TinyLlama/TinyLlama-1.1B-Chat-v0.1',
-            num_params=1,
-			**common_args
+            **common_args
         )
     elif llama_version == 'llama2-7b':
         llm = LLM(
             model_name='meta-llama/Llama-2-7b-chat-hf',
-            num_params=7,
-			**common_args
+            **common_args
         )
     elif llama_version == 'llama3.1-8b':
         llm = LLM(
             model_name='meta-llama/Llama-3.1-8B-Instruct',
-            num_params=8,
-			**common_args
+            **common_args
         )
 
 
